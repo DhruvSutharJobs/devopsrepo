a072ba7ba715a4ab87dda6d28ebe948a1fd55c6d
DhruvSutharJobs <162339786+DhruvSutharJobs@users.noreply.github.com>
2024-03-27 19:51:29 +0530

    running docker

--- app/models.py
+++ app/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Todos(models.Model):
     title= models.CharField(max_length=100)
     text = models.TextField()
+    type = "Todo"
     def __str__(self) -> str:
         return self.title
